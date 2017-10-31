eb0f06519a2ab69f6c13695fb8fe662482bb5ea4
tracek <lukasz.tracewski@gmail.com>
2017-10-31 19:38:48 +0100

    add logging

--- download.py
+++ download.py
@@ -2,16 +2,23 @@ import os
 import re
 import urllib
 import tarfile
+import logging
+from retrying import retry
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 from functools import partial
+from appconfig import setup_logging
 
 
+@retry(stop_max_attempt_number=3, wait_fixed=2000)
 def download_and_extract(target, url):
-    print('Downloading', url)
-    fileobj = urllib.request.urlopen(url)
-    archive = tarfile.open(fileobj=fileobj, mode="r|gz")
-    archive.extractall(target)
+    logging.info('Downloading %s', os.path.basename(url))
+    try:
+        fileobj = urllib.request.urlopen(url)
+        archive = tarfile.open(fileobj=fileobj, mode="r|gz")
+        archive.extractall(target)
+    except Exception as ex:
+        logging.exception('Failed to get %', os.path.basename(url))
 
 
 def get_links(url):
@@ -23,16 +30,19 @@ def get_links(url):
 
 
 def main():
+    setup_logging()
     num_parallel = 8
     voxforge_url = 'http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit'
-    target_folder = '/home/tracek/data/gender/Voxforge'
+    target_folder = '/home/tracek/Data/gender/Voxforge'
 
     if not os.path.isdir(target_folder):
         os.makedirs(target_folder)
 
     links = get_links(voxforge_url)
     download_wrapper = partial(download_and_extract, target_folder)
-    result = Pool(num_parallel).map(download_wrapper, links)
+    for link in links:
+        download_and_extract(target=target_folder, url=link)
+    # result = Pool(num_parallel).map(download_wrapper, links)
 
 if __name__ == '__main__':
     main()
