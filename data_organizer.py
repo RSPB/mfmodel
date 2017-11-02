10ca1d814ecd69e0c74dce037aa769551ba2ad52
tracek <lukasz.tracewski@gmail.com>
2017-11-02 12:24:56 +0100

    convert flac to wave

--- data_organizer.py
+++ data_organizer.py
@@ -1,23 +1,32 @@
 import os
 import glob
+import sox
+import shutil
 import re
+import pathlib
 import collections
 from appconfig import setup_logging
 
 def main():
     setup_logging()
-    target_folder = '/home/tracek/Data/gender/Voxforge'
+
+    genders = {'male', 'female'}
+    download_folder = '/home/tracek/Data/gender/Voxforge'
+    # download_folder = '/home/tracek/Data/gender/test'
+    output_dir = '/home/tracek/Data/gender/raw/'
+    [pathlib.Path(os.path.join(output_dir, gender_dir)).mkdir(parents=True, exist_ok=True) for gender_dir in genders]
 
     cnt = collections.Counter()
-    gender_not_found = []
+    gender_not_found_path = []
 
-    for folder in os.listdir(path=target_folder):
-        if  os.path.isfile(os.path.join(target_folder, folder, 'etc/README')):
-            readme_path = os.path.join(target_folder, folder, 'etc/README')
-        elif os.path.isfile(os.path.join(target_folder, folder, 'etc/readme')):
-            readme_path = os.path.join(target_folder, folder, 'etc/readme')
+    for folder in os.listdir(path=download_folder):
+        source_dir = os.path.join(download_folder, folder)
+        if  os.path.isfile(os.path.join(source_dir, 'etc/README')):
+            readme_path = os.path.join(source_dir, 'etc/README')
+        elif os.path.isfile(os.path.join(source_dir, 'etc/readme')):
+            readme_path = os.path.join(source_dir, 'etc/readme')
         else:
-            raise ValueError('No readme in %s' % readme_path)
+            raise ValueError('No readme in %s' % source_dir)
         with open(readme_path, 'r') as readme:
             found = False
             for line in readme:
@@ -25,25 +34,32 @@ def main():
                 if match:
                     cleanstr = re.sub('\W+', '', match.group(1))
                     gender = cleanstr.lower()
-                    if gender in {'male', 'female'}:
+                    if gender in genders:
+                        dest_dir = os.path.join(output_dir, gender, folder)
+                        waves_source = os.path.join(source_dir, 'wav/')
+                        flac_source  = os.path.join(source_dir, 'flac/')
+
+                        if os.path.isdir(waves_source):
+                            shutil.copytree(waves_source, dest_dir)
+                        elif os.path.isdir(flac_source):
+                            os.makedirs(dest_dir)
+                            tfm = sox.Transformer()
+                            for flakpath in glob.glob(flac_source + '*.flac'):
+                                filename_noext = os.path.splitext(os.path.basename(flakpath))[0]
+                                wave_filename = os.path.join(dest_dir, filename_noext + '.wav')
+                                tfm.build(flakpath, wave_filename)
+                        else:
+                            raise NotImplemented('Missing converter')
+                        shutil.copy(readme_path, dest_dir)
                         found = True
                     cnt[gender] += 1
             if not found:
-                gender_not_found.append(readme_path)
+                gender_not_found_path.append(readme_path)
 
     print('Counter')
     print(cnt)
-    print('Not found:')
-    print(gender_not_found)
+    print('Gender no found in: ', len(gender_not_found_path))
+    [print(gender_missing) for gender_missing in gender_not_found_path]
 
 if __name__ == '__main__':
     main()
-    readme_path = '/home/tracek/Data/gender/Voxforge/kayray-20070604-wha/etc/README'
-    with open(readme_path, 'r') as readme:
-        found = False
-        for line in readme:
-            match = re.search("Gender: (\W*\w+\W*)", line, re.IGNORECASE)
-            if match:
-                print(match.group(1))
-                cleanString = re.sub('\W+', '', match.group(1))
-                print(cleanString)
